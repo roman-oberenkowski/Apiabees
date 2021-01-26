59bcc9b1ce4df7090e387722a932f9cc015b6abb
Roman <roman.oberenkowski@student.put.poznan.pl>
2021-01-26 13:25:13 +0100

    polish characters apiary_code_name

--- app/Http/Livewire/Apiary/Table.php
+++ app/Http/Livewire/Apiary/Table.php
@@ -46,12 +46,15 @@ class Table extends Component
     }
 
     public function openApiaryDeleteModal($id){
+        $id=base64_decode($id);
         $this->emit('openApiaryDeleteModal', $id);
     }
     public function openApiaryDetailsModal($id){
+        $id=base64_decode($id);
         $this->emit('openApiaryDetailsModal', $id);
     }
     public function openApiaryEditModal($id){
+        $id=base64_decode($id);
         $this->emit('openApiaryEditModal', $id);
     }
 }
--- resources/views/livewire/apiary/table.blade.php
+++ resources/views/livewire/apiary/table.blade.php
@@ -93,14 +93,14 @@
 
                     <td class="pr-4 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                         <a href="#" class="text-red-600 hover:text-red-900"
-                           wire:click="openApiaryDeleteModal('{{$apiary->code_name}}')" wire:loading.attr="disabled">
+                           wire:click="openApiaryDeleteModal('{{base64_encode($apiary->code_name)}}')" wire:loading.attr="disabled">
                             <i class="fas fa-times pr-2"></i>Delete</a>
 
                         <a href="#" class="text-gray-400 hover:text-gray-900"
-                           wire:click="openApiaryDetailsModal('{{$apiary->code_name}}')" wire:loading.attr="disabled">
+                           wire:click="openApiaryDetailsModal('{{base64_encode($apiary->code_name)}}')" wire:loading.attr="disabled">
                             <i class="fas fa-address-card pl-2 pr-2"></i>Details</a>
                         <a href="#" class="text-green-400 hover:text-green-900"
-                           wire:click="openApiaryEditModal('{{$apiary->code_name}}')" wire:loading.attr="disabled">
+                           wire:click="openApiaryEditModal('{{base64_encode($apiary->code_name)}}')" wire:loading.attr="disabled">
                             <i class="fas fa-edit pl-2 pr-2"></i>Edit</a>
                     </td>
                 </tr>
