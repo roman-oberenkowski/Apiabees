d69ff8349ab1b907994c53ec4ddd997090237f4d
Roman <roman.oberenkowski@student.put.poznan.pl>
2021-01-26 11:20:54 +0100

    user seeder - admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 
+use App\Models\Employee;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Seeder;
@@ -19,7 +20,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-
+        $f=\Faker\Factory::create('pl_PL');
+        $emp= Employee::create();
+        $emp->PESEL='99999999999';
+        $emp->first_name='Admin';
+        $emp->last_name='Admin';
+        $emp->salary=$f->numberBetween(10,50)*200;
+        $emp->house_number=$f->numberBetween(1,200);
+        $emp->street=substr($f->streetName,0,32);
+        $emp->city=$f->city;
+        if($f->randomDigit%2==0){
+            $emp->appartement=$f->numberBetween(1,50);
+        }
+        $emp->date_of_employment=$f->dateTimeBetween();
+        $emp->save();
+        $userData = [];
+        $userData['name'] = $emp->first_name.' '.$emp->last_name;
+        $userData['email'] = env('ADMIN_EMAIL');
+        $userData['password'] = Hash::make(env('ADMIN_PASSWORD'));
+        //$userData['employee_Pesel'] = $emp->PESEL;
+        $emp->user()->Create($userData);
     }
 }
 
